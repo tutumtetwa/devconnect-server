c9350de7741bf280e210872e7d62c0ab8215c807
Tutu <116677145+tutumtetwa@users.noreply.github.com>
2025-05-22 20:06:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,3 +28,7 @@ app.post('/posts', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+app.get('/', (req, res) => {
+  res.send('Server is up!');
+});
+
